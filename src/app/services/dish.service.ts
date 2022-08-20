0dcc99355d9e7f740b5803c2ff7d40faabe2cb29
Stephen Ubong <stevelov1982@gmail.com>
2022-08-20 17:53:48 +0100

    Rxjs Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +23,8 @@ export class DishService {
   getFeaturedDish(): Observable<Dish> {
     return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
     
+  }
+  getDishIds(): Observable<string[] | any> {
+    return of(DISHES.map(dish => dish.id ));
   }
 }
